de50a46a0e81f14325667f8168cb38dddf11aa1c
Matthew Bauer <krakenthedestroyer@gmail.com>
2019-05-26 18:46:50 -0500

    Created company model and migration

---
+++ app/Company.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    protected $fillable = ['name', 'email', 'logo', 'website'];
+
+//    NOTE: Similar approach but is taking a less secure route
+//    protected $guarded = [];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+}
---
+++ database/migrations/2019_05_26_233239_create_companies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+//            NOTE: Took the initiative to make this unique... Figured it made sense since you wouldn't want dupped companies.
+            $table->string('name')->unique();
+            $table->string('email')->nullable();
+            $table->string('logo')->nullable();
+            $table->string('website')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
