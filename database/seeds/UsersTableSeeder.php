8e28413506e72c557210a3ae29c04d5e83b66463
Matthew Bauer <krakenthedestroyer@gmail.com>
2019-05-26 21:49:29 -0500

    Fixed seeder, removed laradock temporarily due to mysql issues

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,17 +1,17 @@
 <?php
 
-use Illuminate\Database\Seeder;
-
-class UsersTableSeeder
+class UsersTableSeeder extends DatabaseSeeder
 {
     public function run()
     {
         DB::table('users')->insert([
+            'name' => 'admin',
             'email' => 'admin@site.com',
             'password' => 'password'
         ]);
 
         DB::table('users')->insert([
+            'name' => 'manager',
             'email' => 'manager@site.com',
             'password' => 'password'
         ]);
